1dcec9ceb17899bc49774207f1c54ad2ebc7679f
rlyost <ry@ryost.us>
2018-03-09 21:37:26 -0800

    Finished game

--- num.py
+++ num.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, request, redirect, session
 import random
 
 app = Flask(__name__)
@@ -8,40 +8,30 @@ app.secret_key = 'ThisIsSecret'
 def home():
     if not session.get('guessit'):
         session['guessit'] = 0
-    elif session['guessit'] = 0:
         session['guessit'] = random.randrange(1, 101)
-    session['display_result'] = "<div></div>"
-    return render_template('index.html', result_of_guess = session['display_result'])
+        session['display_result'] = "none"
+    elif session['guessit'] == 0:
+        session['guessit'] = random.randrange(1, 101)
+    print session['guessit']
+    return render_template('index.html')
 
 @app.route('/check', methods=['POST'])
 def check():
-    if guess == session['guessit']:
+    if int(request.form['guess']) == session['guessit']:
         # display correct anwser
-        return redirect('/showgot')
-        
-    elif guess > session['guessit']:
+        session['display_result'] = "correct"
+    elif int(request.form['guess']) > session['guessit']:
         # desplay Too High!
-        return redirect('/showhigh')
-        session['display_result'] = "<div></div>"
+        session['display_result'] = "high"
     else:
         # display Too Low!
-        return redirect('/showlow')
-
-@app.route('/showlow')
-def show_low():
-  return render_template('low.html')    
-
-@app.route('/showhigh')
-def show_high():
-  return render_template('high.html')
-
-@app.route('/showgot')
-def show_got():
-  return render_template('got.html')
+        session['display_result'] = "low"
+    return redirect('/')
 
 @app.route('/reset')
 def reset():
     # reset the random number and redirect to the home page
+    session['display_result'] = "none"
     session['guessit'] = 0 
     return redirect('/')
 
